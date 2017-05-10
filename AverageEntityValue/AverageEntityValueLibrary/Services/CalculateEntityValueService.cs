f1da05ae8afa0f2189516b196c91381e73d2e90e
minos55 <marko.thuma@gmail.com>
2017-05-10 15:59:33 +0200

    First comit

---
+++ AverageEntityValue/AverageEntityValueLibrary/Services/CalculateEntityValueService.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Nomnio.AverageEntityValue.Interfaces;
+using Serilog;
+using Microsoft.WindowsAzure.Storage;
+
+namespace Nomnio.AverageEntityValue
+{
+    class CalculateEntityValueService : IEntityValue
+    {
+        private ILogger myLog;
+        private string ConnectionString;
+        private string TableName;
+        CloudStorageAccount StorageAccount;
+
+        public CalculateEntityValueService(string connectionString, string tableName)
+        {
+            myLog = Log.ForContext<CalculateEntityValueService>();
+            ConnectionString = connectionString;
+            TableName = tableName;
+            StorageAccount = CloudStorageAccount.Parse(ConnectionString);
+            myLog.Information("Connected to {Connection}", StorageAccount);
+        }
+
+        public async Task Average()
+        {
+            
+        }
+    }
+}
---
+++ AverageEntityValue/AverageEntityValueLibrary/Services/IEntityValue.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Nomnio.AverageEntityValue.Interfaces
+{
+    public interface IEntityValue
+    {
+        Task Average();
+    }
+}
